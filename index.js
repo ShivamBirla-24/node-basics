26b5058ba5832da8b1a4a213e0467fbbf1c87946
ShivamBirla-24 <shivambirla2403@gmail.com>
2023-11-28 14:49:22 +0530

    added environment variables

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require('ejs')
+const dotenv = require('dotenv')
 const mongoose = require('mongoose')// library which is used to connect mongoDB and Express Server 
-const { error } = require("console")
+
+dotenv.config()
 const app = express()
 
 app.use(bodyParser.urlencoded({extended:false}))
@@ -12,12 +14,12 @@ app.set('view engine','ejs')
 
 
 app.get('/',(req,res)=>{
-    res.send("hello world!")
+    res.send("Hello")
 })
 
 app.listen(3000,()=>{
     mongoose
-   .connect('mongodb+srv://shivam:shivam2403@cluster0.58y41wp.mongodb.net/?retryWrites=true&w=majority')// this is a method used to connect mongoDB and server but this is asynchronous process.Inside this connect method we have to provide the url of our database.
+   .connect(process.env.MONGODB_URL)// this is a method used to connect mongoDB and server but this is asynchronous process.Inside this connect method we have to provide the url of our database.
        .then(()=>{
         console.log('Connection Successful')
        })
